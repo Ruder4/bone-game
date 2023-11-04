f6cf3f3dc82eb823eec8ad2fd97f3fc400de88e4
Ruder4 <ryder.j.anderson2004@gmail.com>
2023-11-03 23:04:44 -0400

    Movement(Improved)

--- Bone sketlon/Assets/PlayerMovement.cs
+++ Bone sketlon/Assets/PlayerMovement.cs
@@ -16,19 +16,27 @@ public class PlayerMovement : MonoBehaviour
     private float jumpBufferTime = 0.2f;
     private float jumpBufferCounter;
 
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMultiplier = 2f;
+
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
+        if (rb.velocity.y < 0)
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        else if (rb.velocity.y > 0 && !Input.GetButton("Jump"))
+        {
+            rb.velocity += Vector2.up * Physics2D.gravity.y * (lowJumpMultiplier - 1) * Time.deltaTime;
+        }
+
+
         horizontal = Input.GetAxisRaw("Horizontal");
         
         if(IsGrounded())
